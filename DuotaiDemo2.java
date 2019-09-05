d09a5582da6d10263e7c05e4c84eae1b2a64c10b
airvip <sdqhwzb@163.com>
2019-09-05 18:07:15 +0800

    duotai

--- DuotaiDemo2.java
+++ DuotaiDemo2.java
@@ -1,11 +1,65 @@
 
 
+interface Pci 
+{
+    public abstract void open();
+    public abstract void close();
+
+}
 
 
+class NetCard implements Pci
+{
+    @Override
+    public void open() {
+        System.out.println("net card open");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("net card close");
+    }
+}
+
+
+class SoundCard implements Pci
+{
+    @Override
+    public void open() {
+        System.out.println("sound card open");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("sound card close");
+    }
+}
+
+
+class MainBoard
+{
+    public void run()
+    {
+        System.out.println("main board runing...");
+    }
+
+    public void usePCI(Pci p) {
+        if(p != null)
+        {
+            p.open();
+            p.close();
+        }
+    }
+}
 
 class DuotaiDemo2
 {
     public static void main(String[] args) {
-        
+        MainBoard mb = new MainBoard();
+        mb.run();
+        mb.usePCI(null);
+        mb.usePCI(new NetCard());
+        mb.usePCI(new SoundCard());
+
     }
 }
