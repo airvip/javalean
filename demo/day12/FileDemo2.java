fad8e2fdf6de7cec6f1b2efcdab8efcac7d121b0
airvip <sdqhwzb@163.com>
2019-12-10 18:11:15 +0800

    fileDemo2

--- demo/day12/FileDemo2.java
+++ demo/day12/FileDemo2.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 /**
  * File类常见方法
@@ -10,7 +12,31 @@ public class FileDemo
 {
     public static void main(String[] args) throws IOException
     {
-        listDemo();
+        File dir = new File("E:\\workspace\\javas\\demo\\day11");
+        File[] files = dir.listFiles();
+        for(File f : files)
+        {
+            sop(f.getName() + "::" + f.length());
+        }
+    }
+
+    public static void listDemo2()
+    {
+        File dir = new File("E:\\workspace\\javas\\demo\\day11");
+        String[] names = dir.list(new FilenameFilter(){
+        
+            @Override
+            public boolean accept(File dir, String name) {
+                // TODO Auto-generated method stub
+                // return !(Pattern.matches("(.*).java$", name));
+                return name.endsWith(".java");
+                // return false;
+            }
+        }); 
+        for(String name : names)
+        {
+            sop(name);
+        }
     }
 
     public static void listDemo()
